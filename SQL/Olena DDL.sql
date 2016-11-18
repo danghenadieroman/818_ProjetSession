127056a092177f89dab94abaad5d13c67eb5cf19
LOlena <1695208@bdeb.qc.ca>
2016-11-18 13:51:02 -0500

    Added SQL DDL files for DB

---
+++ SQL/Olena DDL.sql
@@ -0,0 +1,17 @@
+--drop table users;
+CREATE TABLE users
+  (
+    userno NUMBER(3) CONSTRAINT user_userno_pk PRIMARY KEY,
+    login  VARCHAR2(50),
+    pwd    VARCHAR2(50)
+  );
+CREATE sequence users_seq start with 1 increment BY 1 nocycle;
+  CREATE TABLE userinfo
+    (
+      userno    NUMBER(4) REFERENCES users(userno),
+      nom       VARCHAR2(50),
+      couriel   VARCHAR2(50),
+      zipcode   VARCHAR2(6),
+      telephone VARCHAR2(12)
+    );
+ 
